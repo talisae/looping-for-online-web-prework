84a69015a4ae04f0e051bf8f4147f2f9cb8c6bed
Talisa Erazo <talisaerazo@gmail.com>
2018-10-13 16:10:00 +0000

    automatically backed up by learn

--- for.rb
+++ for.rb
@@ -1,5 +1,13 @@
 
 def using_for
 	checklist = 1..10
+	for item in checklist
+		puts "Wingardium Leviosa"
 	#your code here
+end
+
+shoe_count = 1..32
+ 
+for shoe in shoe_count 
+    puts "I'm putting a pair of shoes away."
 end
